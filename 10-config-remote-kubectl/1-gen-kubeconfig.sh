e39a2cca3535e54bdcc695a2e7e39f3c2f947333
wangying <wangying02@mskj.com>
2019-11-09 20:51:07 +0800

    [fix] Fix typo

--- 10-config-remote-kubectl/1-gen-kubeconfig.sh
+++ 10-config-remote-kubectl/1-gen-kubeconfig.sh
@@ -7,10 +7,10 @@ KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-har
 kubectl config set-cluster kubernetes-the-hard-way \
     --certificate-authority=../ca.pem \
     --embed-certs=true \
-    --server=https:${KUBERNETES_PUBLIC_ADDRESS}:6443
+    --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443
 
 kubectl config set-credentials admin \
-    --client-certificate=../admin.pem
+    --client-certificate=../admin.pem \
     --client-key=../admin-key.pem
 
 kubectl config set-context kubernetes-the-hard-way \
