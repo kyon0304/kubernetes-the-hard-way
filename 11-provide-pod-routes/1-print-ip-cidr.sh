f539508c1976ddc796cfe2ffc25499e864a0855d
wangying <wangying02@mskj.com>
2019-11-09 21:12:18 +0800

    [fix] Fix typos

--- 11-provide-pod-routes/1-print-ip-cidr.sh
+++ 11-provide-pod-routes/1-print-ip-cidr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for instances in worker-0 worker-1 worker-2; do
+for instance in worker-0 worker-1 worker-2; do
     gcloud compute instances describe ${instance} \
         --format 'value[separator=" "](networkInterfaces[0].networkIP,metadata.items[0].value)'
 done
--- 11-provide-pod-routes/2-create-routes.sh
+++ 11-provide-pod-routes/2-create-routes.sh
@@ -2,7 +2,7 @@
 
 for i in 0 1 2; do
     gcloud compute routes create kuberentes-route-10-200-${i}-0-24 \
-        --network kuberentes-the-hard-way \
+        --network kubernetes-the-hard-way \
         --next-hop-address 10.240.0.2${i} \
         --destination-range 10.200.${i}.0/24
 done
